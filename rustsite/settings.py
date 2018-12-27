b2b306179e027d2471a6d50848104c57e1782159
Aayush Chaudhry <iit2017135@gmail.com>
2018-12-27 17:06:52 +0530

    hello

--- rustsite/settings.py
+++ rustsite/settings.py
@@ -20,7 +20,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join('root/app/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
